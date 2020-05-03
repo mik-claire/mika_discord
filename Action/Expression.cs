69606c54d7da45f20065eca0d023f86463f6c119
mik-claire <mekakushi.mkz@gmail.com>
2020-05-03 16:54:06 +0900

    Implemented expression reaction.

---
+++ Action/Expression.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mika_discord.Action
+{
+    public static class Expression
+    {
+        private static Random rand = new Random();
+        private static readonly List<string> randomMessages = new List<string>
+        {
+            "どういたしましてっ！",
+            "わーい！ふふ、もっと褒めてくださっても良いんですよ？"
+        };
+
+        public static string GetExpression()
+        {
+            return randomMessages[getRandom(randomMessages)];
+        }
+
+        private static int getRandom(List<string> randomMessages)
+        {
+            return rand.Next(0, randomMessages.Count);
+        }
+    }
+}
---
+++ Reaction/ExpressionReaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using mika_discord.Action;
+
+namespace mika_discord.Reaction
+{
+    public class ExpressionReaction : ReactionBase
+    {
+        public ExpressionReaction()
+        {
+            Keywords = new List<string>() { "ありがと", "thx", "thanks" };
+        }
+
+        public override async Task Execute(ISocketMessageChannel channel, SocketUser user, params string[] args)
+        {
+            await channel.SendMessageAsync(GetMentionPrefix(user) + Expression.GetExpression());
+        }
+    }
+}
